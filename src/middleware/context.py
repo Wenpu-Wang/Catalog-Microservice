8f75312689fb9b237efce021f0ab5abdc2a5821e
Vanpul <ww2569@columbia.edu>
2022-12-23 00:32:07 -0500

    Update context.py

--- src/middleware/context.py
+++ src/middleware/context.py
@@ -3,7 +3,7 @@ import os
 import pymysql
 
 SUBSCRIPTIONS = {"/items"}
-SLACK_URL = "https://hooks.slack.com/services/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"
+SLACK_URL = "https://hooks.slack.com/services/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"
 SNS_ARN = "arn:aws:sns:us-east-1:381693958687:catalog_item_request"
 
 
